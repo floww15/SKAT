0ed56acb8cd1a8034a024f80ccac68815885dfc3
FFX721 <49677342+FFX721@users.noreply.github.com>
2019-06-22 13:36:47 +0200

    Anpassungen der GUI durch externe Mitarbeiter

--- src/ReizenActivity.java
+++ src/ReizenActivity.java
@@ -3,8 +3,11 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class ReizenActivity extends Application {
@@ -20,39 +23,40 @@ public class ReizenActivity extends Application {
 
 		Label lNR = new Label("Spieler NR:");
 		Label lGereizt = new Label("Gereizt bis: ");
-		Button btnWeg = new Button("Weg");
-		btnWeg.setPrefSize(80, 20);
 		Button btnNext = new Button("'nächster Wert'");
 		btnNext.setPrefSize(80, 20);
 
-		GridPane pane2 = new GridPane();
-		pane2.setVgap(1);
-		pane2.setHgap(1);
-		pane2.add(lNR, 0, 0);
-		pane2.add(lGereizt, 1, 0);
-		pane2.add(btnWeg, 0, 1);
-		pane2.add(btnNext, 1, 1);
-
-		GridPane pane1 = new GridPane();
-		pane1.setVgap(1);
-		pane1.setHgap(0);
-		pane1.add(pane2, 0, 0);
+		VBox rechts = new VBox();
+		rechts.getChildren().add(lGereizt);
+		rechts.getChildren().add(btnNext);
 		
-
+		Button btnWeg = new Button("Weg");
+		btnWeg.setPrefSize(80, 20);
+		VBox links = new VBox();
+		links.getChildren().add(lNR);
+		links.getChildren().add(btnWeg);
+		
+		TextField field = new TextField();
+		field.setText("Florian mag FX");
+		
+		HBox hbox = new HBox();
+		hbox.setSpacing(20);
+		hbox.getChildren().addAll(links,rechts,field);
+		hbox.setAlignment(Pos.CENTER);
 		Label labelHeader = new Label("SKAT");
 		labelHeader.setStyle("-fx-font-size:2em");
 
 		BorderPane borderPane = new BorderPane();
 		borderPane.setTop(labelHeader);
 		BorderPane.setAlignment(labelHeader, Pos.CENTER);
-		borderPane.setCenter(pane1);
-		BorderPane.setAlignment(pane1, Pos.CENTER);
-		
+		borderPane.setCenter(hbox);
 		
 		Scene scene = new Scene(borderPane, 600,400);
 		primaryStage.setTitle("Skat");
 		primaryStage.setScene(scene);
 		primaryStage.show();
+
+		field.setMinHeight(primaryStage.getHeight()/2);
 
 	}
 
