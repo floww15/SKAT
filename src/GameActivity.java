f74465dccaf248442317b0f73211dfe06a47105f
floww15 <florian.weymar@web.de>
2019-06-24 09:17:49 +0200

    Deleted Warnings

--- src/GameActivity.java
+++ src/GameActivity.java
@@ -1,15 +1,16 @@
 
-import java.awt.Panel;
+
+
 
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
+
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
+
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
