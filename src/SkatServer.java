646b11436f8a7d5c927a870818427cd4c646a385
floww15 <florian.weymar@web.de>
2019-06-24 08:31:03 +0200

    Client / Server

--- src/SkatServer.java
+++ src/SkatServer.java
@@ -3,7 +3,12 @@
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
-public class SkatServer extends UnicastRemoteObject{
+public class SkatServer extends UnicastRemoteObject implements RemoteSkatServer{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	protected SkatServer() throws RemoteException {
 		super();
--- src/StartActivity.java
+++ src/StartActivity.java
@@ -1,5 +1,6 @@
 
 import javafx.application.Application;
+
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -7,13 +8,18 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class StartActivity extends Application {
-	
+	Button btnConnect;
+	TextField tfIP, tfName;
+
 	public static void main(String[] args) {
 		Application.launch(args);
+	}
+
+	StartActivity() {
+	
 	}
 
 	@Override
@@ -22,39 +28,45 @@ public class StartActivity extends Application {
 		primaryStage.setResizable(false);
 //		Button btnConnect = new Button("Connect");
 //		btnConnect.setPrefSize(80,20);
-		Button btnStart = new Button("Connect");
-		btnStart.setPrefSize(80,20);
-		TextField tfIP = new TextField();
-		TextField tfName = new TextField();
-		Label label1= new Label("Server IP:");
-		Label label2= new Label("Spielername:");
-		
-		Label labelHeader= new Label("SKAT");
+		btnConnect = new Button("Connect");
+		btnConnect.setPrefSize(80, 20);
+		tfIP = new TextField();
+		tfName = new TextField();
+		Label label1 = new Label("Server IP:");
+		Label label2 = new Label("Spielername:");
+
+		Label labelHeader = new Label("SKAT");
 		labelHeader.setStyle("-fx-font-size:2em");
 //		labelHeader.setAlignment(Pos.CENTER);
-		
-		BorderPane borderPane=new BorderPane();
+
+		BorderPane borderPane = new BorderPane();
 		borderPane.setTop(labelHeader);
 		BorderPane.setAlignment(labelHeader, Pos.CENTER);
 
-		GridPane pane= new GridPane();
+		GridPane pane = new GridPane();
 		pane.setHgap(2);
 		pane.setVgap(2);
 //		pane.add(btnConnect, 2,1);
-		pane.add(btnStart, 2,2);
-		pane.add(tfIP, 1,1);
-		pane.add(tfName, 1,2);
+		pane.add(btnConnect, 2, 2);
+		pane.add(tfIP, 1, 1);
+		pane.add(tfName, 1, 2);
 		pane.add(label1, 0, 1);
 		pane.add(label2, 0, 2);
 //		pane.add(labelHeader, 1, 0);
 		pane.setAlignment(Pos.CENTER);
 		borderPane.setCenter(pane);
-		Scene scene = new Scene(borderPane, 700,430);
+		Scene scene = new Scene(borderPane, 700, 430);
 		primaryStage.setTitle("SKAT");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
-	
-	
+
+	public String getIP() {
+		return tfIP.getText();
+	}
+
+	public String getName() {
+		return tfName.getText();
+	}
 
 }
