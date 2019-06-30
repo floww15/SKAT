5743666d4260e818b1212b022a749f5b8ebe9e79
MarcHender <50866280+MarcHender@users.noreply.github.com>
2019-06-30 10:07:45 +0200

    Comparator der Karten, um Gewinner eines Stiches zu bestimmen

--- src/GameClasses/Karte.java
+++ src/GameClasses/Karte.java
@@ -1,6 +1,7 @@
 package GameClasses;
 
 import java.io.Serializable;
+import java.util.Comparator;
 
 public class Karte implements Serializable {
 	/**
@@ -10,6 +11,7 @@ public class Karte implements Serializable {
 	private String farbe;
 	private String wert;
 	private int punkte;
+
 	public Karte(String f, String w) {
 		farbe = f;
 		wert = w;
@@ -44,11 +46,137 @@ public class Karte implements Serializable {
 		return wert;
 	}
 
+	public int getWertAsInt() {
+		return Integer.parseInt(wert);
+	}
+
 	public String toString() {
 		return farbe + " " + wert;
 	}
 
 	public int getPoints() {
 		return punkte;
+	}
+
+	public int getFarbeAsInt() {
+		if (farbe.equals("Kreuz"))
+			return 3;
+		else if (farbe.equals("Pik"))
+			return 2;
+		else if (farbe.equals("Herz"))
+			return 1;
+		else
+			return 0;
+	}
+
+	class KartenComparator implements Comparator<Karte> {
+		String trumpf;
+		String farbe;
+
+		KartenComparator(String wert, String farbe) {
+			trumpf = wert; // Trumpf bzw. was gespielt wird
+			this.farbe = farbe; // Farbe die beantwortet werden musste
+		}
+
+		@Override
+		public int compare(Karte k1, Karte k2) {
+			// TODO Auto-generated method stub
+			if (trumpf.equals("Grand")) { /** Wenn Grand gespielt wird **/
+				if (k1.getWert().equals("Bube")) { /** Falls k1 ein Bube ist **/
+					if (!k2.getWert().equals("Bube")) {
+						return 1; /** Falls k2 kein Bube ist **/
+					} else if (k1.getFarbeAsInt() > k2.getFarbeAsInt()) { /** Falls k2 auch ein Bube ist **/
+						return 1;
+					} else
+						return -1;
+				} else {
+					if (k2.getWert().equals("Bube")) {
+						return -1; /** Falls k2 ein Bube ist, aber k1 nicht **/
+					}
+					else {
+						if (k1.getPoints() > k2.getPoints())
+							return 1; /** Falls die erste Karte der höhere Trumpf ist **/
+						else if (k1.getPoints() < k2.getPoints())
+							return -1; /** Falls die zweite Karte der höhere Trumpf ist **/
+						else {
+							if (k1.getWertAsInt() > k2.getWertAsInt()) {
+								return 1; /** Wenn beide Zahlen als Karten haben und k1 höher ist **/
+							} else
+								return -1; /** Wenn beide Zahlen als Karten haben und k2 höher ist **/
+						}
+					}
+				}
+			} else if (trumpf.equals("Null")) { /** Wenn Null gespielt wird **/
+				if (k1.getPoints() > k2.getPoints())
+					return 1; /** Falls die erste Karte der höhere Trumpf ist **/
+				else if (k1.getPoints() < k2.getPoints())
+					return -1; /** Falls die zweite Karte der höhere Trumpf ist **/
+				else {
+					if (k1.getWertAsInt() > k2.getWertAsInt()) {
+						return 1; /** Wenn beide Zahlen als Karten haben und k1 höher ist **/
+					} else
+						return -1; /** Wenn beide Zahlen als Karten haben und k2 höher ist **/
+				}
+			} else {
+				if (k1.getWert().equals("Bube")) { /** Falls k1 ein Bube ist **/
+					if (!k2.getWert().equals("Bube")) {
+						return 1; /** Falls k2 kein Bube ist **/
+					} else if (k1.getFarbeAsInt() > k2.getFarbeAsInt()) { /** Falls k2 auch ein Bube ist **/
+						return 1;
+					} else
+						return -1;
+
+				} else {
+					if (k2.getWert().equals("Bube"))
+						return -1; /** Falls k2 ein Bube ist, aber k1 nicht **/
+					else { /** Falls beide keine Buben sind **/
+						if (!k1.getFarbe().equals(trumpf)
+								&& !k1.getFarbe().equals(farbe)) { /** Falls k1 Abwerfkarte ist **/
+							if (!k2.getFarbe().equals(trumpf) && !k2.getFarbe().equals(farbe)) {
+								return 0; /** Falls beide Karten abwerfkarten sind **/
+							} else {
+								return -1; /** Falls nur die erste Karte eine Abwerfkarte ist **/
+							}
+						} else { /** Falls k1 keine Abwerfkarte ist **/
+							if (!k2.getFarbe().equals(trumpf) && !k2.getFarbe().equals(farbe)) {
+								return 1; /** Falls nur die zweite Karte eine Abwerfkarte ist **/
+							} else {
+								if (k1.getFarbe().equals(trumpf)) { /** Falls k1 Trumpf ist **/
+									if (!k2.getFarbe().equals(trumpf)) {
+										return 1; /** Falls nur die erste Karte trumpf ist **/
+									} else {
+										if (k1.getPoints() > k2.getPoints())
+											return 1; /** Falls die erste Karte der höhere Trumpf ist **/
+										else if (k1.getPoints() < k2.getPoints())
+											return -1; /** Falls die zweite Karte der höhere Trumpf ist **/
+										else {
+											if (k1.getWertAsInt() > k2.getWertAsInt()) {
+												return 1; /** Wenn beide Zahlen als Karten haben und k1 höher ist **/
+											} else
+												return -1; /** Wenn beide Zahlen als Karten haben und k2 höher ist **/
+										}
+									}
+								} else { /** Falls k1 die Farbe beantwortet **/
+									if (!k2.getFarbe().equals(farbe)) { /** Falls k2 nicht die Farbe beantwortet **/
+										return 1; /** Falls nur die erste Karte richtige Farbe ist **/
+									} else { /** Falls k2 die Farbe auch beantwortet **/
+										if (k1.getPoints() > k2.getPoints())
+											return 1; /** Falls die erste Karte die höhere Karte ist **/
+										else if (k1.getPoints() < k2.getPoints())
+											return -1; /** Falls die zweite Karte die höhere Karte ist **/
+										else {
+											if (k1.getWertAsInt() > k2.getWertAsInt()) {
+												return 1; /** Wenn beide Zahlen als Karten haben und k1 höher ist **/
+											} else
+												return -1; /** Wenn beide Zahlen als Karten haben und k2 höher ist **/
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 }
