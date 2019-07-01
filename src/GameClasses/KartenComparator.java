ef2ee1c5308d1bd98ab5aa8ac2ed416568d1f35b
David Petry <David Petry@192.168.0.17>
2019-07-01 12:30:43 +0200

    Game erweitert, KartenComparator nicht mehr als innere Klasse, Constructor von Player verändert, Klasse Stich verändert

---
+++ src/GameClasses/KartenComparator.java
@@ -0,0 +1,136 @@
+package GameClasses;
+
+import java.util.Comparator;
+
+public class KartenComparator implements Comparator<Karte> {
+		String trumpf;
+		String farbe;
+
+		public KartenComparator(String trumpf, String farbe) {
+			this.trumpf = trumpf; // Trumpf bzw. was gespielt wird
+			this.farbe = farbe; // Farbe die beantwortet werden musste
+		}
+
+		@Override
+		public int compare(Karte k1, Karte k2) {
+			// TODO Auto-generated method stub
+			if (trumpf.equals("Grand")) { /** Wenn Grand gespielt wird **/
+				if (k1.getWert().equals("Bube")) { /** Falls k1 ein Bube ist **/
+					if (!k2.getWert().equals("Bube")) {
+						return -1; /** Falls k2 kein Bube ist **/
+					} else if (k1.getFarbeAsInt() > k2.getFarbeAsInt()) { /** Falls k2 auch ein Bube ist **/
+						return -1;
+					} else
+						return 1;
+				} else {
+					if (k2.getWert().equals("Bube")) {
+						return 1; /** Falls k2 ein Bube ist, aber k1 nicht **/
+					} else {
+						if (k1.getPoints() > k2.getPoints())
+							return -1; /** Falls die erste Karte der höhere Trumpf ist **/
+						else if (k1.getPoints() < k2.getPoints())
+							return 1; /** Falls die zweite Karte der höhere Trumpf ist **/
+						else {
+							if (k1.getWertAsInt() > k2.getWertAsInt()) {
+								return -1; /** Wenn beide Zahlen als Karten haben und k1 höher ist **/
+							} else
+								return 1; /** Wenn beide Zahlen als Karten haben und k2 höher ist **/
+						}
+					}
+				}
+			} else if (trumpf.equals("Null")) { /** Wenn Null gespielt wird **/
+
+				if (!k1.getFarbe().equals(farbe)) { /** Wenn k1 nicht die Farbe ist **/
+					if (k2.getFarbe().equals(farbe))
+						return 1;
+					else
+						return -1;
+				} else { /** Wenn k1 die Farbe ist **/
+
+					if (!k2.getFarbe().equals(farbe)) { /** Falls k2 nicht die Farbe beantwortet **/
+						return -1; /** Falls nur die erste Karte richtige Farbe ist **/
+					} else { /** Falls k2 die Farbe auch beantwortet **/
+						if (k1.getWert().equals("10")) { /** 10er werden eingereiht **/
+							if (k2.getPoints() > 1)
+								return 1; /** Falls k2 Bube oder Höher **/
+							else
+								return -1;
+						} else if (k2.getWert().equals("10")) {
+							if (k1.getPoints() > 1)
+								return -1;
+							else
+								return 1;
+						} else if (k1.getPoints() > k2.getPoints())
+							return -1; /** Falls die erste Karte die höhere Karte ist **/
+						else if (k1.getPoints() < k2.getPoints())
+							return 1; /** Falls die zweite Karte die höhere Karte ist **/
+						else {
+							if (k1.getWertAsInt() > k2.getWertAsInt()) {
+								return -1; /** Wenn beide Zahlen als Karten haben und k1 höher ist **/
+							} else
+								return 1; /** Wenn beide Zahlen als Karten haben und k2 höher ist **/
+						}
+					}
+				}
+			} else {
+				if (k1.getWert().equals("Bube")) { /** Falls k1 ein Bube ist **/
+					if (!k2.getWert().equals("Bube")) {
+						return -1; /** Falls k2 kein Bube ist **/
+					} else if (k1.getFarbeAsInt() > k2.getFarbeAsInt()) { /** Falls k2 auch ein Bube ist **/
+						return -1;
+					} else
+						return 1;
+
+				} else {
+					if (k2.getWert().equals("Bube"))
+						return 1; /** Falls k2 ein Bube ist, aber k1 nicht **/
+					else { /** Falls beide keine Buben sind **/
+						if (!k1.getFarbe().equals(trumpf)
+								&& !k1.getFarbe().equals(farbe)) { /** Falls k1 Abwerfkarte ist **/
+							if (!k2.getFarbe().equals(trumpf) && !k2.getFarbe().equals(farbe)) {
+								return -1; /** Falls beide Karten abwerfkarten sind **/
+							} else {
+								return 1; /** Falls nur die erste Karte eine Abwerfkarte ist **/
+							}
+						} else { /** Falls k1 keine Abwerfkarte ist **/
+							if (!k2.getFarbe().equals(trumpf) && !k2.getFarbe().equals(farbe)) {
+								return -1; /** Falls nur die zweite Karte eine Abwerfkarte ist **/
+							} else {
+								if (k1.getFarbe().equals(trumpf)) { /** Falls k1 Trumpf ist **/
+									if (!k2.getFarbe().equals(trumpf)) {
+										return -1; /** Falls nur die erste Karte trumpf ist **/
+									} else {
+										if (k1.getPoints() > k2.getPoints())
+											return -1; /** Falls die erste Karte der höhere Trumpf ist **/
+										else if (k1.getPoints() < k2.getPoints())
+											return 1; /** Falls die zweite Karte der höhere Trumpf ist **/
+										else {
+											if (k1.getWertAsInt() > k2.getWertAsInt()) {
+												return -1; /** Wenn beide Zahlen als Karten haben und k1 höher ist **/
+											} else
+												return 1; /** Wenn beide Zahlen als Karten haben und k2 höher ist **/
+										}
+									}
+								} else { /** Falls k1 die Farbe beantwortet **/
+									if (!k2.getFarbe().equals(farbe)) { /** Falls k2 nicht die Farbe beantwortet **/
+										return -1; /** Falls nur die erste Karte richtige Farbe ist **/
+									} else { /** Falls k2 die Farbe auch beantwortet **/
+										if (k1.getPoints() > k2.getPoints())
+											return -1; /** Falls die erste Karte die höhere Karte ist **/
+										else if (k1.getPoints() < k2.getPoints())
+											return 1; /** Falls die zweite Karte die höhere Karte ist **/
+										else {
+											if (k1.getWertAsInt() > k2.getWertAsInt()) {
+												return -1; /** Wenn beide Zahlen als Karten haben und k1 höher ist **/
+											} else
+												return 1; /** Wenn beide Zahlen als Karten haben und k2 höher ist **/
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
--- src/SpielAblauf/Game.java
+++ src/SpielAblauf/Game.java
@@ -1,6 +1,33 @@
 package SpielAblauf;
 
+import GameClasses.Karte;
+import GameClasses.KartenComparator;
+import GameClasses.Stich;
+import GameClasses.Player;
+
 public class Game {
+	private boolean[] gelegt=new boolean[3];
+	private Karte[] cards=new Karte[3];
+	private Player[] player=new Player[3];
+ 	
 	
+	public void karteLegen(int id,Karte k,String trumpf,Player p) {
+		if(!gelegt[id]) {
+			cards[id]=k;
+			player[id]=p;
+		}
+		if(gelegt[0]&&gelegt[1]&& gelegt[2]) {
+			Stich s=new Stich(cards[0],cards[1],cards[2]);
+			KartenComparator comp=new KartenComparator(trumpf,cards[0].getFarbe());
+			if(comp.compare(cards[0], cards[1])>0&&comp.compare(cards[0], cards[2])>0)
+				player[0].addStich(s);
+			if(comp.compare(cards[1], cards[0])>0&&comp.compare(cards[1], cards[2])>0)
+				player[1].addStich(s);
+			if(comp.compare(cards[2], cards[1])>0&&comp.compare(cards[2], cards[0])>0)
+				player[2].addStich(s);
+			
+		}
+		
+	}
 
 }
